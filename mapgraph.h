54dc93f3223380ce67f0deebc9280cbf1b2c300c
SheikhWalter <2023170341@cis.asu.edu.eg>
2025-05-19 23:16:03 +0300

    Optimized Dijkstra using Multisource Bidirectional Dijkstra

--- mapgraph.h
+++ mapgraph.h
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 #include <queue>
-#define priorityQueue std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, std::greater<std::pair<double, int>>>
+#define priorityQueue std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, std::greater<>>
 
 struct Node {
     int id;
@@ -37,7 +37,8 @@ public:
     PathResult findShortestPath(double startX, double startY, double endX, double endY, double R);
     std::string compareWithOutput(const std::string& outputFilename);
 
-    std::vector<std::pair<int, double>> findNodesWithinRadius(double x, double y, double endx, double endy, double R, priorityQueue& pq, std::vector<double>& time, std::vector<double>& dist);
+    std::vector<std::pair<int, double>> findNodesWithinRadius(double x, double y, double endx, double endy, double R, std::priority_queue<std::pair<double, int>, std::vector<std::
+                                                              pair<double, int>>, std::greater<>> &pq, std::vector<double> &time, std::vector<double> &dist);
 
     std::vector<PathResult> runAllQueries();
 
